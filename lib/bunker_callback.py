600fcd9e4265b6988325e8ad3104b44b1e86c314
octavioaguila <oeaguila@uc.cl>
2026-01-19 21:38:01 -0300

    feat: CL in progress

--- lib/bunker_callback.py
+++ lib/bunker_callback.py
@@ -51,9 +51,9 @@ class CurriculumCallback(BaseCallback):
         
         # Define your curriculum levels
         self.levels = [
-            {"max_goal_sampling_distance": 4.0, "energy_weight": 0.0,  "threshold": 0.8}, # In the first level the only parameter that matters is threshold, the rest is just for compatibility
-            {"max_goal_sampling_distance": 8.0, "energy_weight": 0.0, "threshold": 0.8},
-            {"max_goal_sampling_distance": 12.0, "energy_weight": 0.0, "threshold": 0.8},
+            {"max_goal_sampling_distance": 4.0, "threshold": 0.8}, # In the first level the only parameter that matters is threshold, the rest is just for compatibility
+            {"max_goal_sampling_distance": 8.0, "threshold": 0.8},
+            {"max_goal_sampling_distance": 12.0, "threshold": 0.8},
         ]
 
     def _on_step(self) -> bool:
@@ -73,22 +73,16 @@ class CurriculumCallback(BaseCallback):
                 new_cfg = self.levels[self.current_level]
                     
                 print(f"\n[Curriculum] LEVEL UP! Now at Level {self.current_level}\n")
-                print(f"Updating: max_goal_sampling_distance={new_cfg['max_goal_sampling_distance']}, energy_weight={new_cfg['energy_weight']}\n")
+                print(f"Updating: max_goal_sampling_distance={new_cfg['max_goal_sampling_distance']}\n")
                 
                 # Update all vectorized environments (Training & Eval)
                 self._update_envs(new_cfg)
-        
-        # Log curriculum metrics
-        current_cfg = self.levels[self.current_level]
-        self.logger.record("curriculum/level", self.current_level)
-        self.logger.record("curriculum/max_goal_sampling_distance", current_cfg["max_goal_sampling_distance"])
-        self.logger.record("curriculum/energy_weight", current_cfg["energy_weight"])
         
         return True
 
     def _update_envs(self, cfg):
         # We must use env_method or set_attr for SubprocVecEnv
         # Update Training Envs
-        self.training_env.env_method("set_curriculum", cfg["max_goal_sampling_distance"], cfg["energy_weight"])
+        self.training_env.env_method("set_curriculum", cfg["max_goal_sampling_distance"])
         # Update Eval Envs
-        self.eval_env.env_method("set_curriculum", cfg["max_goal_sampling_distance"], cfg["energy_weight"])
+        self.eval_env.env_method("set_curriculum", cfg["max_goal_sampling_distance"])
